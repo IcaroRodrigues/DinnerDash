ca54b0f1713cf4db5719ee05f922f3c470618f47
Icaro Rodrigues <icaro.cerqueira@cjr.org.br>
2020-06-14 11:25:09 -0300

    adiciona metodos de requisição nos controllers

--- app/controllers/order_meals_controller.rb
+++ app/controllers/order_meals_controller.rb
@@ -1,4 +1,45 @@
 class OrderMealsController < ApplicationController
-    def new
+
+  before_action :set_meal, only: [:show, :update, :destroy]
+
+  def index
+    @meal_categories = Meal_category.all
+    render json:@meal_categories, status: 200
+  end
+ 
+  def show
+    render json:@meal_category, status: 200
+  end
+
+  def create
+    @meal_category = Meal_category.new(meal_params)
+    if @Meal_category.save
+      render json:@meal_category, status: 201
+    else
+      render json:@Meal_category.errors, status: :unprocessable_entity
+    end   
+  end
+
+  def update
+    if @meal_category.update(meal_params)
+      render json: @meal_category, status: 200
+    else
+      render json: @Meal_category.errors, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @meal_category.destroy
+  end
+
+  private
+
+  def set_meal
+    @mealCategory = Meal_category.find(params[:id])
+  end
+
+  def meal_params
+    params.require(:meal_category).permit( :name )
+  end
+
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users
-  resources :meals
-  resources :meal_categories
-  resources :situations
-  resources :orders
-  resources :order_meals
+  
+  
+  get "/meals", to: "meals#index"
+  get "/meals/:id", to: "meals#show"
+  post "/meals", to: "meals#create"
+  put "/meals/:id", to: "meals#update"
+  delete "meals/:id", to: "meals#destroy"
+
+  get "/meals_category", to: "meals_category#index"
+  
 end
