0dec23bebba7ac2b45ea2ecf8a94c983eb2c5e64
Ana Paula Nobrega <ana.costa@cjr.org.br>
2020-06-17 21:20:07 -0300

    com as seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,8 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+sobremesas = MealCategory.create(name: 'Sobremesas')
+ creme_brule = Meal.create(name: 'Crème Brûlée', description: 'Classico creme de baunilha maçaricado', price: '70', available: true, meal_category_id: 1)
+ petit_gateau = Meal.create(name: 'Petit gateau', description: 'Bolo gostoso com sorvete', price: '50', available: true, meal_category_id: 1)
+ torta_limao = Meal.create(name: 'Torta de Limão', description: 'Torta gostosa', price: '50', available: true, meal_category_id: 1)
