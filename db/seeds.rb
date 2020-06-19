aa3875f3d127ddabd06c049cbc6faad2699da0ac
Ana Paula Nobrega <ana.costa@cjr.org.br>
2020-06-18 23:54:33 -0300

    preco e povoamento dos seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,30 @@
 #
 # Examples:
 
+
 sobremesas = MealCategory.create(name: 'Sobremesas')
  creme_brule = Meal.create(name: 'Crème Brûlée', description: 'Classico creme de baunilha maçaricado', price: '70', available: true, meal_category_id: 1)
  petit_gateau = Meal.create(name: 'Petit gateau', description: 'Bolo gostoso com sorvete', price: '50', available: true, meal_category_id: 1)
- torta_limao = Meal.create(name: 'Torta de Limão', description: 'Torta gostosa', price: '50', available: true, meal_category_id: 1)
+ torta_limao = Meal.create(name: 'Torta de Limão', description: 'Torta gostosa com limão', price: '50', available: true, meal_category_id: 1)
+
+situations = Situation.create([
+    {description: 'Pendente'},
+    {description: 'Entregue'},
+    {description: 'Cancelado'}
+])
+
+orders = Order.create([
+    {situation_id:1},
+    {situation_id:1},
+    {situation_id:1}
+])
+orderMeals = OrderMeal.create([
+    {quantity:2, order_id: 1, meal_id: 1}
+])
+
+
+
+
+
+
+
